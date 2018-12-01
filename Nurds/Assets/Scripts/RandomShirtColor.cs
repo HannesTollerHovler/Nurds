1c460764b2e8f263c9ee5ca2bf05c7dc1e106005
HannesTollerHovler <hannesth03@gmail.com>
2018-12-01 22:29:52 +0100

    Random shirt color bresh

---
+++ Nurds/Assets/Scripts/RandomShirtColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomShirtColor : MonoBehaviour {
+
+	public SpriteRenderer body;
+	public Color[] color;
+
+	void Awake(){
+		int maxIndex = color.Length;
+		int randomIndex = Random.Range(0, maxIndex);
+		body.color = color[randomIndex];
+
+	}
+}
